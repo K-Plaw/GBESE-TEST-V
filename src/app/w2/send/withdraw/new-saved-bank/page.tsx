0c7c2010bac43fa5263fbde5cd3cced67a938aa4
K-Plaw <praislaw@gmail.com>
2025-05-10 14:13:04 +0100

    Mock code for new-saved-bank.tsx

---
+++ src/app/w2/send/withdraw/new-saved-bank/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const NewSavedBank: React.FC = () => {
+  return (
+    <div>
+      <h1>Add a New Bank</h1>
+      {/* Your form or content goes here */}
+    </div>
+  );
+};
+
+export default NewSavedBank;
