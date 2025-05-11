ecb1325379e74dcc00d832188eab81f98be2e898
K-Plaw <praislaw@gmail.com>
2025-05-11 23:07:08 +0100

    Retry v deploy fixing

--- src/app/w2/send/withdraw/review-details/page.tsx
+++ src/app/w2/send/withdraw/review-details/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic"; // this line to prevent prerendering
+
 import React from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 
